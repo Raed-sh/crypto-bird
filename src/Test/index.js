ff5bc43a02ef53f1f4e396266469cca2793001d6
Mr. KJ <master@asp-api.com>
2021-08-09 20:28:30 +0300

    fixed gameplay

--- src/Test/index.js
+++ src/Test/index.js
@@ -1,20 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import useWindowDimensions from '../helpers/windowDims';
 import bird from '../images/flappy-bird.png'
 import './styles.css'
 
 export default function Test() {
     // Each Obstacle will be an object { 'pos': int, 'height': int }
+    const { height, width } = useWindowDimensions();
+    const [didPopulate, setDidPopulate] = useState(false);
+    const[bPos, setBPos] = useState(50)
+    console.log(height, width)
     const [obstacles, setObstacles] = useState([
-        {pos:1200, height: 150}, 
-        {pos:850,height:200}, 
-        {pos:510,height:30},
-        {pos:150, height:250},
     ]);
 
+
+
+        function jump(){
+            setBPos(bPos+1);
+        }
+
+        function prePopulate(){
+            setDidPopulate(true)
+            var temp_obs = []
+            for (var i=0; i<9; i++){
+                temp_obs.push({pos:i*width/10, height:getRandomInt(30,(height/2)-(height/7))})
+            }
+            setObstacles(temp_obs)
+        }
         useEffect(() => {
+            if (!didPopulate){
+                prePopulate()
+            }
             const updateInterval = setInterval(() => {
             updateObstacles();
-            }, 150);
+            
+            }, 300);
             // const addInterval = setInterval(() => {
             //     addObstacle();
             // }, 100);
@@ -33,29 +52,37 @@ export default function Test() {
         
         // add a new obstacle to the array with a defined starting pos in this case 200
         function generateObstacle(){
-            return {pos: 500, height:getRandomInt(30,300)} 
+            return {pos: (width-(width/20)), height:getRandomInt(30,(height/2)-(height/8))} 
         };
 
+        function drop(){
+            setBPos(bPos-3)
+            if (bPos <= 0){
+                // endGame();
+            }
+        }
+
+        function checkCollision(){
+            if (obstacles[0].height >= bPos){
+                //endGame();
+            }
+        }
 
         // update obstacles to make them move and add new ones
         function updateObstacles(){
+            drop();
+            checkCollision();
             var temp_obstacles = [...obstacles]
             for (var i=0; i<temp_obstacles.length; i++){
-                if (temp_obstacles[i].pos <= 100){
+                if (temp_obstacles[i].pos < 10){
                     temp_obstacles.splice(0,1)
+                    temp_obstacles.push(generateObstacle())
+                    break;
                 }
-                else{
-                    var new_obs = {...temp_obstacles[i]};
-                    new_obs.pos -= 20;
-                    temp_obstacles[i] = new_obs;
-                }
+                temp_obstacles[i] = {...temp_obstacles[i], pos:temp_obstacles[i].pos-35};
             } 
-            console.log(temp_obstacles)
-            if (temp_obstacles.length <= 4){
-                setObstacles([...temp_obstacles, generateObstacle()])
-            }else{
+            
                 setObstacles(temp_obstacles)
-            }
         };
 
         // remove the closest obstacle from the left and adds a new one to the right
@@ -66,8 +93,8 @@ export default function Test() {
         };
 
     return(
-        <div className="testbg">
-            <img src={bird} />
+        <div className="testbg" onClick={() => jump()}>
+            <img src={bird} style={{position:'absolute',  bottom:bPos.b}}/>
             {obstacles.map((o) =>
             <> 
                 <div className="obst t" style={{ height:o.height, position:'absolute',left:o.pos, top:0,  transition: 'left 100ms ease-in-out'}}/>
